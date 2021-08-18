095277b5092d47904360a504148b0fe378edaa3c
Alex Kleider <alexkleider@protonmail.com>
2021-08-18 13:36:13 -0700

    csvconverter.py now working

--- csvconverter.py
+++ csvconverter.py
@@ -10,65 +10,31 @@ converts the information into a csv file
 (for the benefit of future excel loving officials.)
 """
 
+import sys
 import csv
 import utils
 import data
 import rbc
 
+EXCLUDED_STATI = {'m', 'zae'}
+
 club = rbc.Club()
-sponsor_data = data.get_sponsor_data(club.SPONSORS_SPoT)
-sponsor_keys = sorted(sponsor_data.keys())
-applicant_data = data.get_applicant_data(
-    club.APPLICANT_SPoT, all_dates=True)
+applicant_data = data.get_applicant_data(club.APPLICANT_SPoT,
+                                         club.SPONSORS_SPoT)
 applicant_keys = sorted(applicant_data.keys())
-print("\nSponsor Data")
-for key in sponsor_data.keys():
-    print("{}: {}".format(key, sponsor_data[key]))
-# print(sponsor_data)
-print("\nApplicant Data")
-for key in applicant_data.keys():
-    print("{}: {}".format(key, applicant_data[key]))
-# print(applicant_data)
-applicant_data_lines = []
-for key in applicant_keys:
-    csv_line = []
-    names = key.split(', ')
-    csv_line.append(names[1])
-    csv_line.append(names[0])
-    csv_line.append(applicant_data[key]['status'])
-#   print("possible keys are ", applicant_data[key].keys())
-    if 'dates' in applicant_data[key].keys():
-        for date in applicant_data[key]['dates']:
-            csv_line.append(date)
-    while len(csv_line) < 8:
-        csv_line.append('')
-    if key in sponsor_keys:
-        csv_line.extend(list(sponsor_data[key]))
-    applicant_data_lines.append(csv_line)
+#print("\nApplicant Data")
+#for key in applicant_data.keys():
+#    print("{}: {}\n".format(key, applicant_data[key]))
+#sys.exit()
 
-response = input(
-        "Include all data? (vs only current applicants.) (Y/N)")
-
-applicant_data_field_names = ("first", "last", "status", 
-    "First", "Second", "Third", "ApprovalDate", "DateDuesPaid",
-    "Sponsor1", "Sponsor2",)
 
 with open(club.APPLICANT_CSV, 'w', newline='') as fileobj:
-    writer = csv.writer(fileobj,
-            dialect='unix', quoting=csv.QUOTE_NONE)
-    writer.writerow(applicant_data_field_names)
-    for line in applicant_data_lines:
-        if response and response[0] in {'y', 'Y'}:
-            writer.writerow(line)
-        else:
-            if line[2] != 'm':
-                writer.writerow(line)
-print("\nApplicant_data:")
-print(applicant_data_field_names)
-for line in applicant_data_lines:
-    if response and response[0] in {'y', 'Y'}:
-        print(line)
-    else:
-        if line[2] != 'm':
-            print(line)
+    dict_writer = csv.DictWriter(fileobj,
+                                 rbc.Club.APPLICANT_DATA_FIELD_NAMES, 
+                                 dialect='unix',
+                                 quoting=csv.QUOTE_MINIMAL)
+    dict_writer.writeheader()
+    for key in applicant_keys:
+        if not applicant_data[key]['status'] in EXCLUDED_STATI:
+            dict_writer.writerow(applicant_data[key])
 
--- data.py
+++ data.py
@@ -136,6 +136,7 @@ def gather_contacts_data(club):
                 club.g_by_group[key].add(g_dict["gname"])
 
 
+redactedAug18_2021 = '''
 def parse_applicant_line4dates(line,
                                bad_line_list=None,
                                expired_applicant_list=None,
@@ -194,7 +195,7 @@ def parse_applicant_line4dates(line,
     if len(parts) > 3:  # waste dates of induction and membership.
         parts = parts[:3]
     return (name, parts)
-
+'''
 
 def move_date_listing_into_record(dates, record):
     try:
@@ -248,7 +249,7 @@ def applicant_data_line2record(line):
     elif l == 5:
         status = "a3"
     elif l == 6:
-        status = "ai"
+        status = "ad"
     elif l == 7:
         status = "m"
     else: assert(False)
@@ -333,17 +334,7 @@ def parse_sponsor_data_line(line):
     sponsors = tuple([
         sponsor.strip() for sponsor in parts[1].split(", ")])
     ret = name, sponsors
-#   print("Parse line returning {}".format(repr(ret)))
     return ret
-# above replaces ?buggy? following code
-    parts = line.split(":")
-    sponsored = parts[0].strip()
-    names = sponsored.split()
-    name = '{}, {}'.format(names[1], names[0])
-    sponsors = tuple([
-        sponsor.strip() for sponsor in parts[1].split(", ")])
-    key, value = name, sponsors
-    return (key, value)
 
 
 def get_sponsor_data(spot):
@@ -354,61 +345,35 @@ def get_sponsor_data(spot):
     ret = {}
     with open(spot, 'r') as src:
         print('Reading file "{}"...'.format(src.name))
-#       lines = [
-#           line for line in helpers.useful_lines(src, comment='#')]
-#       for line in lines:
         for line in helpers.useful_lines(src, comment='#'):
-#           print("Line is '{}".format(line))
             tup = parse_sponsor_data_line(line)
-#           print("tup is {}".format(repr(tup)))
             (name, sponsors) = (tup[0], tup[1])
             ret[name] = sponsors
     return ret
-# Above replaces the following ?buggy? code
-    ret = {}
-    with open(spot, 'r') as src:
-        print('Reading file "{}"...'.format(src.name))
-        for name, sponsors in parse_sponsor_data_line(
-                            helpers.useful_lines(src, comment='#')):
-            ret[name] = sponsors
-    return ret
 
 
-def get_applicant_data(spot, sponsor_file=None, all_dates=False):
+def get_applicant_data(spot, sponsor_file=None):
     """
     Reads spot, the applicant data file +/- the sponsor file.
     Returns a dict keyed by applicant names ("last, first").
-    Values are dicts keyed by
-        "status" (value is one of the first 5 or last 2 
-            listed keys in STATUS_KEY_VALUES
-            Be ware of the last 2: not on path for membership
-            and not expected to be in the member DB.
-        "dates" (value a tuple of dates[1]) and (if 'sponsor_file)
-        "sponsors" (value a 2 tuple of strings- names of sponsors.
-    [1] by default only (first 3) meetings attended are listed.
-    If <all_dates> then all 7 dates are included:
-    App rcv'd, App fee rcv', 1st..3rd meetings, Exec appr'd, Fees paid
+    Values are records (dicts) with fields as defined by
+    rbc.Club.APPLICANT_DATA_FIELD_NAMES.
     """
+    if sponsor_file:
+        sponsor_data = get_sponsor_data(sponsor_file)
+        sponsored_applicants = set(sponsor_data.keys())
     ret = {}
     with open(spot, 'r') as src:
         print('Reading file "{}"...'.format(src.name))
-        if sponsor_file:
-            sponsors = get_sponsor_data(sponsor_file)
-            sponsored_members = sponsors.keys()
         for line in helpers.useful_lines(src, comment='#'):
-            res = parse_applicant_data_line(line)  # Returns None
-            if res: key, value = res               # if no longer
-            else: continue                         # an applicant.
-#           print("app_data: {} {}".format(key, repr(value)))
-            status = value[0]
-            if status.startswith('z'):
-                continue
-            ret[key] = {'status': status}
-            if len(value) > 1:  # active applicant
-                ret[key]['dates'] = value[1:]
-        for applicant in ret.keys():
-            if sponsor_file and 'dates' in ret[applicant].keys():
-                ret[applicant]["sponsors"] = sponsors[applicant]
+            res = applicant_data_line2record(line)
+            name = member.get_last_first(res)
+            ret[name] = res
+            if sponsor_file and name in sponsored_applicants:
+                sponsors = sponsor_data[name]
+                assert len(sponsors) == 2
+                ret[name]['Sponsor1'] = sponsors[0].strip()
+                ret[name]['Sponsor2'] = sponsors[1].strip()
     return ret
 
 
@@ -881,13 +846,14 @@ def ck_data(club,
 #   if temp_ret:
 #       ret.append("\nNon Applicant Stati: {}"
 #           .format(','.join(temp_ret)))
-    a_applicants = helpers.keys_removed(a_applicants, ('m',))
+    a_applicants = helpers.keys_removed(a_applicants,
+                                        ('m', 'zae'))
     a_applicantsets = helpers.lists2sets(a_applicants)
     ms_by_status_sets = helpers.lists2sets(club.ms_by_status)
-#   ms_by_status_sets = member.filter_out_stati(ms_by_status_sets, 'm')
+#   ms_by_status_sets = helpers.keys_removed(ms_by_status_sets,
+#                                            ('m', 'zae'))
     if a_applicantsets != ms_by_status_sets:
         ret.append("\nApplicant problem:")
-        ret.append("(OK to ignore if only 'ai' vs 'ad')")
         ret.append("The following data from applicant SPoT-")
         ret.extend(helpers.show_dict(a_applicants, extra_line=False))
         ret.append("- does not match the following membership SPot-")
